c0e6a683936f193ef5ef6349814ea1543f7ea863
Humberto Leal <humberto.adolfo@hotmail.com>
2019-11-07 16:08:00 -0500

    Start script updates and mongodb connection in server file along with express

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express, { RequestHandler, Response } from 'express'
+import Routes from "./routes";
+require('./config/mongoose');
 
 const app :express.Application = express();
+
+app.use(express.json());
+
+app.use('/api', Routes);
 
 app.get('/', (req, res) => {
     res.send("Hellow world!");
@@ -8,4 +14,4 @@ app.get('/', (req, res) => {
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000');
-})
+});
