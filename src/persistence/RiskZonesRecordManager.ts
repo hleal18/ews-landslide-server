41c825e8b2bc8a425282def83ea7c21e4e7da227
Humberto Leal <humberto.adolfo@hotmail.com>
2019-11-07 19:05:57 -0500

    Get and edit methods in RiskZonesRecordManager

--- src/persistence/RiskZonesRecordManager.ts
+++ src/persistence/RiskZonesRecordManager.ts
@@ -2,13 +2,25 @@ import { RiskZones } from "./RiskZone";
 import IRiskZone from "../model/IRiskZone";
 
 class RiskZonesRecordManager {
-    static async addRiskZone(zone: IRiskZone) : Promise<IRiskZone> {
-        const result : IRiskZone = await RiskZones.create(zone);
+    static async addRiskZone(zone: IRiskZone): Promise<IRiskZone> {
+        const result: IRiskZone = await RiskZones.create(zone);
         return result;
     }
 
-    static async getRiskZone(id_zone: String) : Promise<IRiskZone | null> {
-        const result : IRiskZone | null = await RiskZones.findById({_id: id_zone});
+    static async getRiskZone(id_zone: String): Promise<IRiskZone | null> {
+        const result: IRiskZone | null = await RiskZones.findById(id_zone);
+        return result;
+    }
+
+    static async editRiskZone(id_zone: String, zone: IRiskZone): Promise<IRiskZone | null> {
+        const result: IRiskZone | null = await RiskZones.findByIdAndUpdate(id_zone, zone, { new: true });
+        if (result) {
+            console.log('Entry updated, new zone: ', result);
+        }
+        else {
+            console.log('Entry not updated, parameter: ', zone);
+        }
+
         return result;
     }
 }
