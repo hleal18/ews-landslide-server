ee71ea1e9f4a9d76dd6d9f16b41c95a0d667333f
Humberto Leal <humberto.adolfo@hotmail.com>
2019-11-07 19:06:56 -0500

    Support for Get and Edit RiskZones in controllers and routes

--- src/controllers/RiskZonesControllers.ts
+++ src/controllers/RiskZonesControllers.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import RiskZonesManager from "../service/RiskZonesManager";
 import mongoose from "mongoose";
+import { CLIENT_RENEG_LIMIT } from "tls";
 
 export default class RiskZonesControllers {
     static async addRiskZone(req: Request, res: Response): Promise<void> {
         try {
             console.log('Received body: ', req.body);
             const result = await RiskZonesManager.addRiskZone(req.body);
-            res.status(200).send({ result });
+            res.status(200).send({ riskzone: { result } });
         } catch (e) {
             if (e.name === 'ValidationError') {
                 console.log('There is a missing field in the body of the request: ', req.body);
@@ -19,4 +20,46 @@ export default class RiskZonesControllers {
             }
         }
     }
+
+    static async getRiskZone(req: Request, res: Response): Promise<void> {
+        try {
+            const id: string = req.params["id"];
+            console.log('Received id, ', id);
+            const zone = await RiskZonesManager.getRiskZone(id);
+            if (zone) res.status(200).send({ riskzone: { zone } });
+            else res.status(205).send({ message: `Risk zone with ${id} id not found!` });
+        } catch (e) {
+            if (e.name === 'ValidationError') {
+                console.log('There is a missing filed in the request: ', e.message);
+                res.status(404).send({ message: `Id parameter expected` });
+            }
+            else if (e.name == 'CastError' && e.kind == 'ObjectId') {
+                console.log(`Error: ${e.message}`);
+                res.status(404).send({ message: `Invalid Id received.` });
+            }
+        }
+    }
+
+    static async editRiskZone(req: Request, res: Response): Promise<void> {
+        try {
+            const zoneId = req.params['id'];
+            const zoneAttributes = req.body;
+            const newZone = await RiskZonesManager.editRiskZone(zoneId, zoneAttributes);
+            if (newZone) {
+                res.status(200).send({ riskZone: { newZone } });
+            }
+            else {
+                res.status(404).send({ message: 'Document was not edited, review the request.' });
+            }
+            // Proveer un Id inválido.
+            // Proveer un Id valido y en el cuerpo tratar de modificar Id (es inmutable)
+            // Proveer un id valid pero un atributo id invalido
+            // Modifica directamente las listas internas.
+        } catch (e) {
+            console.log('An error occured: ', e);
+            console.log('An error occured: ', e.message);
+            res.status(404).send({ message: 'Document was not edited, an unexpected error occurred' });
+        }
+    }
+
 }
--- src/routes/RiskZonesRoutes.ts
+++ src/routes/RiskZonesRoutes.ts
@@ -4,5 +4,7 @@ import RiskZonesControllers from "../controllers/RiskZonesControllers";
 const router = Express.Router();
 
 router.post('/', RiskZonesControllers.addRiskZone);
+router.get('/:id', RiskZonesControllers.getRiskZone);
+router.patch('/:id', RiskZonesControllers.editRiskZone);
 
 export default router;
