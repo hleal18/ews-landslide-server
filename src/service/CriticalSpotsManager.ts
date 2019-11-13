32a86b26081bb2b7c5ed488248f09d04805d7279
Humberto Leal <humberto.adolfo@hotmail.com>
2019-11-12 19:58:48 -0500

    Service for CriticalSpots

---
+++ src/service/CriticalSpotsManager.ts
@@ -0,0 +1,33 @@
+import CriticalSpotsRecordManager from '../persistence/CriticalSpotsRecordManager';
+import RiskZonesManager from './RiskZonesManager';
+import ICriticalSpot from '../model/ICriticalSpot';
+import { CriticalSpotDocument } from '../persistence/CriticalSpot';
+
+export default class CriticalSpotsManager {
+    static async addCriticalSpot(riskZoneId: string, criticalSpot: ICriticalSpot): Promise<ICriticalSpot> {
+
+        const newCriticalSpot = await CriticalSpotsRecordManager.addCriticalSpot(criticalSpot);
+        try {
+            const updatedRiskZone = await RiskZonesManager.addCriticalSpotId(riskZoneId, ((newCriticalSpot as CriticalSpotDocument)._id));
+            if (!updatedRiskZone) {
+                // Delete newCriticalSpot
+                await ((newCriticalSpot as CriticalSpotDocument).remove());
+                throw new Error(`Critical Spot with not added, riskZoneId: ${riskZoneId}, not valid`);
+            }
+        } catch (e) {
+            await ((newCriticalSpot as CriticalSpotDocument).remove());
+        }
+
+        return newCriticalSpot;
+    }
+
+    static async getCriticalSpot(criticalSpotId: string): Promise<ICriticalSpot | null> {
+        const criticalSpot: ICriticalSpot | null = await CriticalSpotsRecordManager.getCriticalSpot(criticalSpotId);
+        return criticalSpot;
+    }
+
+    static async editCriticalSpot(criticalSpotId: string, criticalSpot: ICriticalSpot): Promise<ICriticalSpot | null> {
+        const updatedCriticalSpot: ICriticalSpot | null = await CriticalSpotsRecordManager.editCriticalSpot(criticalSpotId, criticalSpot);
+        return updatedCriticalSpot;
+    }
+}
