736339c6cac5648eb86d541fdafda81bb6aeebf8
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-17 11:13:21 -0500

    Password hashing of new user

--- src/service/UsersManager.ts
+++ src/service/UsersManager.ts
@@ -1,9 +1,24 @@
+import Config from '../config/config';
 import IUser from '../domain/IUser';
 import UserRecordManager from '../persistence/UsersRecordManager';
 
+import bcrypt from 'bcrypt';
+
 export default class UsersManager {
     static async addUser(user: IUser): Promise<IUser> {
-        const result: IUser = await UserRecordManager.addUser(user);
+        const duplicate: IUser | null =
+            await UserRecordManager.getUserByEmail(user.email);
+
+        let result: IUser | null = null;
+
+        const userToCreate = user;
+
+        userToCreate.password = bcrypt.hashSync(user.password, Config.auth.bcryptRounds);
+
+        if (!duplicate) result = await UserRecordManager.addUser(userToCreate);
+        else
+            throw new Error(`User provided with email ${user.email} already in use.`);
+
         return result;
     }
 
