a52af36874da6bfdaf4b48e62555a0ff1a52f906
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-21 08:39:03 -0500

    Passport and jwt config params

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,25 @@ dotenv.config();
 interface Config {
     mongo: {
         uri: string
+    },
+    auth: {
+        bcryptRounds: number,
+        jwt_secret: string,
+        jwt_algorithm: string,
+        jwt_expiration: number
     }
 }
 
-export const config : Config = { 
-    mongo: { 
-        uri: ((process.env["MONGO_URI"]) ? process.env["MONGO_URI"] : "")
+const config: Config = {
+    mongo: {
+        uri: process.env["MONGO_URI"] || ""
+    },
+    auth: {
+        bcryptRounds: Number(process.env["BCRYPT_ROUNDS"]),
+        jwt_algorithm: process.env["JWT_ALGORITHM"] || "HS256",
+        jwt_secret: process.env["JWT_SECRET"] || "",
+        jwt_expiration: Number(process.env["JWT_EXPIRATION"])
     }
 };
+
+export default config;
