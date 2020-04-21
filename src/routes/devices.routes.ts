fa2a71cddebc69e85008d3bf8c618aeb554b8dbf
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-21 08:49:03 -0500

    authentication for devices endpoints

--- src/routes/devices.routes.ts
+++ src/routes/devices.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import DevicesController from "../controllers/devices.controller";
+import passport from "passport";
 
 const router: Router = Router();
 
-router.get('/', DevicesController.getDevices);
-router.post('/', DevicesController.addDevice);
-router.get('/:id', DevicesController.getDevice);
-router.patch('/:id', DevicesController.addVariable);
+router.get('/', passport.authenticate('jwt', { session: false }), DevicesController.getDevices);
+router.post('/', passport.authenticate('jwt', { session: false }), DevicesController.addDevice);
+router.get('/:id', passport.authenticate('jwt', { session: false }), DevicesController.getDevice);
+router.patch('/:id', passport.authenticate('jwt', { session: false }), DevicesController.addVariable);
 
 export default router;
