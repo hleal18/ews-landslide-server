001091f5f41024b68478868b115f1764387c0d33
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-21 08:43:11 -0500

    mongoose config updated

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import util from "util";
-import { config } from "./config";
+import Config from "./config";
 
 
-let mongoUri = config.mongo.uri;
+let mongoUri = Config.mongo.uri;
 
-mongoose.connect(mongoUri, {useNewUrlParser: true},(err) => {
+mongoose.connect(mongoUri, { useNewUrlParser: true }, (err) => {
     if (err) console.log(`Error: ${err.message}`);
 });
 
@@ -13,4 +13,4 @@ mongoose.connection.on('error', () => {
     throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
-mongoose.connection.on('connected', () => {console.log(`Successfully connected to MongoDB.`);})
+mongoose.connection.on('connected', () => { console.log(`Successfully connected to MongoDB.`); })
