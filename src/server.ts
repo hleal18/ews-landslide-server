efa77d16bd23fe1d813709a91407af1e6173ccb6
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-21 08:42:21 -0500

    server with config passport before using routes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import Routes from "./routes";
+
+import passport from 'passport';
 
 require('./config/mongoose');
 require('./async-controllers');
+require('./config/passport');
+
 
 const app: express.Application = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
+import Routes from "./routes";
 app.use('/api', Routes);
 
 app.get('/', (req, res) => {
     res.send("Hellow world!");
 });
 
-app.listen(3001, function () {
+app.listen(3002, function () {
     console.log('Example app listening on port 3001');
 });
