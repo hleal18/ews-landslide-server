a9aaec2c0b25f2ceacef4e6911ff1d72685ba2e2
Humberto Leal <humberto.adolfo@hotmail.com>
2020-09-12 23:06:36 -0500

    comments deleted

--- src/config/mailgun.ts
+++ src/config/mailgun.ts
@@ -1,24 +1,6 @@
 import { NodeMailgun } from "ts-mailgun";
 import config from "./config";
 
-// const sendSampleMessage = async () => {
-//   const mailer = new NodeMailgun();
-//   console.log("Apikey :", config.mailgun.apiKey);
-//   mailer.apiKey = "194936308023379ab52648776da4e41b-ee13fadb-250f3bae";
-//   mailer.domain = "email.hleal.email";
-//   mailer.fromEmail = "alertas-sat@email.hleal.email";
-//   mailer.fromTitle = "SAT Alertas";
-//   mailer.unsubscribeLink = false;
-
-// 	mailer.init();
-  
-//   try {
-//     await mailer.send("humberto.adolfo@hotmail.com", "Hello", "Testing some mailgun awesomness!");
-//   } catch(e) {
-//     console.log("Problem when sending email :", e.message);
-//   }
-// }
-
 const mailer = new NodeMailgun(config.mailgun.apiKey, config.mailgun.domain);
 
 mailer.fromEmail = "alertas-sat@email.hleal.email";
@@ -27,14 +9,4 @@ mailer.unsubscribeLink = false;
 
 mailer.init();
 
-// const sendSampleMessage = async ( )  => {
-// 	console.log('Sending message');
-//   try {
-//     await mailer.send("humberto.adolfo@hotmail.com", "Hello", "Testing some mailgun awesomness!");
-//   } catch(e) {
-//     console.log("Problem when sending email :", e.message);
-//   }
-// }
-
-// sendSampleMessage();
 export default mailer;
