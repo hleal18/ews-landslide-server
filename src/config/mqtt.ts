9607cb27358653cbed5ecc183485fa2be52d3771
Humberto <humberto.adolfo@hotmail.com>
2020-11-15 16:11:34 -0500

    mqtt topic and broker connection updated

--- src/config/mqtt.ts
+++ src/config/mqtt.ts
@@ -1,7 +1,6 @@
 import mqtt from "mqtt";
 
-const topics = "hola";
-
+const topics = ["ews-landslide-test"];
 const mqttClient = mqtt.connect('mqtt://semard.com.co', { port: 1883});
 
 console.log('Connecting...............');
@@ -10,15 +9,15 @@ mqttClient.on('connect', () => {
     console.log('Successfully connected: ');
 });
 
-mqttClient.subscribe(topics, (err, granted) => {
+mqttClient.subscribe(topics, { qos: 2 }, (err, granted) => {
     if (err) console.log(`Not subscribed to: `, err);
 
     console.log('Subscribed to: ', granted);
 });
 
-mqttClient.on('message', (topic, payload) => {
-    console.log('Message received for topic: ', topic);
-});
+// mqttClient.on('message', (topic, payload) => {
+//     console.log('Message received for topic: ', topic);
+// });
 
 
 export default mqttClient;
